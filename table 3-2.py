b406e8a117e4bdbce48eab1b7d76e9285b88149d
saboorakhalil-star <saboorakhail@gmail.com>
2026-02-03 12:49:13 +0500

    new file hai

---
+++ table 3-2.py
@@ -0,0 +1,110 @@
+# Loops in python!
+# 
+# icteration , repection word ( application use in c scrpt, css, synctes ka sytle diffrent  
+#  fundamentals:   
+# while  # sister of if  (condition true ho) , repectetive hoga, 
+# for exmaple:
+# a=10
+# if a>=10:
+#             print("1st line")
+#             print("2nd line")                 
+#             print ("3rd line")
+#             print("4th line")
+            
+# a=10
+# while a>10:
+#     print("1st line")
+#     print("2nd line")                 
+#     print ("3rd line")
+#     print("4th line")
+# for  #
+
+
+# num=int(input("Enter a number of table you want to print:"))
+# a=1
+# while a<=10:
+#     print (f" {num} * {a} = {num*a}")
+#     a=a+1
+    
+# num=int(input("Enter a number of table you want to print:"))
+# a=1
+# while a<=10:
+#      print(f" {num}  * {a} = {num*a}")
+#      a=a+1
+        
+# num=int(input("Enter a number of table you want to print:"))
+# a=10
+# while a>=1:
+#      print(f" {num}  * {a} = {num*a}")
+#      a=a-1
+
+
+# boolean : True False
+
+# while True :
+#      print("Hello")
+#      
+#      
+# control loops statments:
+# break
+# continue
+# pass # option
+
+
+# while True:
+#     userid= input("Enter your user ID:")
+#     password=input("Enter your password:")
+#     if (userid=="123" or userid=="1234" or userid=="0000") and pssword=="abcd":
+#         print("Welcome t the system")
+#         rerun=input("Do you want to continue?") .lower()
+#         if rerun=="no":
+#             break
+#         else:
+#             print("ok lets go!")
+# else:
+#     print("Invalid User!")
+#     rerun=input("Do you want to try again?").lower()
+#     if rerun=="no":
+# #         break
+#     else:
+#         print("ok,   !")
+#         continue  
+
+
+# while True:
+#     userid= input("Enter your user ID:")
+#     password=input("Enter your password:")
+#     if (userid=="123" or userid=="1234" or userid=="0000") and password=="abcd":
+#         print("Welcome to the system")
+#         break
+#     else:
+#         print("Invalid User!")
+#         rerun=input("Do you want to try again?").lower()
+#         if rerun=="no":
+#             break
+#             print("")
+#         else:
+#             print("Ok!")
+#             continue
+        
+        
+        
+        
+        
+while True:
+     userid=input("Enter your user ID:")
+     password=input("Enter your password:")
+     if(userid=="saba" or userid=="sabakhan" or userid=="sabakhan123") and password=="87590":
+         print("Welcome to the system")
+         break
+     else:
+         print("invalid User!")
+         rerun=input("Do you want to try again?").lower()
+         if rerun=="no":
+             break
+             print("")
+         else:
+            print("ok!")
+            continue
+         
+      
